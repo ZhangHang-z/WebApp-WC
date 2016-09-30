764b64d61c75e653a6c9f996ffb3cf87aa6b059a
zhanghang <stevezhang2014@gmail.com>
2016-09-30 13:43:13 +0800

    add setting style

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,7 +1,20 @@
 // mine.js
 
 var dataMine = {
-  name: "我的"
+  name: "我的",
+  notificationInfo: {
+    text: "通知",
+    iconUrl: "../../image/icon/notification.png"
+  },
+  addInfo: [{
+    iconUrl: "../../image/icon/userAddress.png",
+    text: "我的地址",
+    navigatorTitle: "toaddress"
+  }, {
+    iconUrl: "../../image/icon/favoriteExpressage.png",
+    text: "常用快递",
+    navigatorTitle: "tofavoriteexpressage"
+  }]
 }
 
 Page({
