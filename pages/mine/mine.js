900097a7d06a748ff9a26364dc49a16702257612
steve zhang <steve zhang>
2016-09-30 23:59:23 +0800

    add icons

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -9,11 +9,28 @@ var dataMine = {
   addInfo: [{
     iconUrl: "../../image/icon/userAddress.png",
     text: "我的地址",
-    navigatorTitle: "toaddress"
+    navigatorTitle: "toaddress",
+    nextLineWXSS: "hasNextLine"
   }, {
     iconUrl: "../../image/icon/favoriteExpressage.png",
     text: "常用快递",
-    navigatorTitle: "tofavoriteexpressage"
+    navigatorTitle: "tofavoriteexpressage",
+    nextLineWXSS: ""
+  }],
+  feedbackInfo: [{
+    iconUrl: "../../image/icon/feedback.png",
+    text: "建议反馈",
+    navigatorTitle: "tofeedback",
+    nextLineWXSS: "hasNextLine"
+  },{
+    iconUrl: "../../image/icon/grade.png",
+    text: "赏个分吧",
+    navigatorTitle: "tograde"
+  }],
+  aboutInfo: [{
+    iconUrl: "../../image/icon/about.png",
+    text: "关于我们",
+    navigatorTitle: "toabout",
   }]
 }
 
